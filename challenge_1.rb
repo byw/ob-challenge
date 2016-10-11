5ca84b83cba9dc6f37383a3850a63df6b5693de8
Bobby Wang <wangbobby@gmail.com>
2016-10-11 17:46:55 -0400

    Implement challenge 1.

---
+++ challenge_1.rb
@@ -0,0 +1,50 @@
+require 'csv'
+require 'bigdecimal'
+
+module Challenge1
+
+  def self.calculate_spreads(csv_data)
+
+    puts 'bond,benchmark,spread_to_benchmark'
+
+    c_bonds, g_bonds = parse_csv(csv_data)
+    c_bonds.each do |c_bond|
+      benchmark = g_bonds.min_by do |g_bond|
+        (g_bond[:term] - c_bond[:term]).abs
+      end
+
+      spread = c_bond[:yield] - benchmark[:yield]
+      printf "%s,%s,%.2f%\n", c_bond[:bond], benchmark[:bond], spread
+    end
+
+  rescue CSV::MalformedCSVError
+    $stderr.puts 'Malformed CSV file!'
+  end
+
+  def self.parse_csv(csv_data)
+    c_bonds, g_bonds = [], []
+    CSV.parse(csv_data, headers: true) do |row| 
+      case row['type']
+      when 'corporate'
+        c_bonds << parse_row(row)
+      when 'government'
+        g_bonds << parse_row(row)
+      end
+    end
+    return c_bonds, g_bonds
+  end
+
+  def self.parse_row(row)
+    {
+      bond: row['bond'],
+      term: BigDecimal(row['term'][0..-6]),
+      yield: BigDecimal(row['yield'][0..-2])
+    }
+  end
+
+end
+
+if file_path = ARGV.first
+  csv_file = File.open(file_path)
+  Challenge1.calculate_spreads(csv_file.read)
+end
---
+++ challenge_1_spec.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'bundler/setup'
+require_relative 'challenge_1'
+
+RSpec.describe Challenge1 do
+  describe '#calculate_spreads' do
+    context 'properly-formed CSV' do
+      let :csv_data do
+        File.open('sample_input.csv').read
+      end
+
+      it 'prints the header row' do
+        expect do
+          Challenge1.calculate_spreads csv_data
+        end.to output(/^bond,benchmark,spread_to_benchmark\n/).to_stdout
+      end
+
+      it 'prints the first data row correctly' do
+        expect do
+          Challenge1.calculate_spreads csv_data
+        end.to output(/\nC1,G1,1.60%\n/).to_stdout
+      end
+
+      it 'prints the last data row correctly' do
+        expect do
+          Challenge1.calculate_spreads csv_data
+        end.to output(/\nC7,G6,2.50%$/).to_stdout
+      end
+    end
+
+    context 'malformed CSV' do
+      it 'prints to stderr that the file is malformed' do
+        malformed_csv = File.open('malformed_csv.csv').read
+        expect do
+          Challenge1.calculate_spreads malformed_csv
+        end.to output("Malformed CSV file!\n").to_stderr
+      end
+    end
+  end
+end
